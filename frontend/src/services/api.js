88ea8d9abfec13f858b259c08005557565518963
Sudhita Reddy <sudhitareddy582@gmail.com>
2026-06-01 08:25:14 +0530

    check the api issue

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://tak-manager-9cns.onrender.com",
+  baseURL: "https://tak-manager-9cns.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
